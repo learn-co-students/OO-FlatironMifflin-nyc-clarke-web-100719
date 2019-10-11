2c520b0d844d8d81de7f690d91aece7790b70176
Ryan <rngao97@gmail.com>
2019-10-11 14:58:30 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,36 @@
 class Employee
+    @@all = []
+
+    attr_accessor :name, :salary, :manager_name
+    def initialize(name, salary, manager_name)
+        @name = name
+        @salary = salary
+        @manager_name = manager_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(salary)
+        @@all.select do |employee|
+            employee.salary > salary
+        end
+    end
+
+    def self.find_by_department(department)
+        @@all.find do |employee|
+           employee.manager_name.department
+        end
+    end
+
+    def tax_bracket
+        @@all.select do |employee| 
+           if (employee.salary - self.salary).abs < 1000
+           employee
+           end
+        end
+    end
 
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,40 @@
 class Manager
+    attr_accessor :name, :department, :age
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @@all << self
+    end
+
+    def employees
+        Employee.all.select do |employee| 
+            employee.manager_name == self
+        end
+    end
+
+    def self.array
+        @@all
+    end
+
+    def hire_employee(name, salary)
+        employees << Employee.new(name, salary, self)
+    end
+
+    def self.all_departments
+        @@all.each do |manager|
+            manager.department
+        end
+    end
+
+    def self.average_age
+        age = 0.0
+        @@all.each do |manager|
+            age += manager.age 
+        end
+        return age/@@all.length
+    end
 
 end
