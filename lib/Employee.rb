2433be4bf3b6d6b09b2264d087c55361f0498186
DannyBrito <britodanny1997@gmail.com>
2019-10-12 15:42:19 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,37 @@
 class Employee
 
+    attr_accessor :name, :salary, :manager, :department
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name, manager, salary) #manager is assumed to be an instance of the Manager class and not a simple string
+        @name = name
+        @manager = manager
+        @department = manager.department # directing as manager can only have one department and
+        @salary = salary
+
+        self.class.all << self
+    end
+
+    def manager_name
+        manager.name
+    end
+
+    def self.paid_over(salary)
+        all.select{|employee| employee.salary > salary}
+    end
+
+    def self.find_by_department(department)
+        all.find{|employee| employee.department == department}
+    end
+
+    def tax_bracket
+        max = self.salary + 1000
+        min = self.salary - 1000
+        self.class.all.select{|employee| employee.salary <= max && employee.salary >= min}.reject{|employee| employee == self}
+    end
+
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,34 @@
 class Manager
 
+    attr_accessor :name, :department, :age
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name, age, department)
+        @name = name
+        @age = age
+        @department = department
+        self.class.all << self
+    end
+
+    def employees
+        Employee.all.select{|employee| employee.manager == self}
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name, self, salary)
+    end
+
+    def self.all_departments
+        all.map{|manager| manager.department}
+    end
+
+    def self.average_age
+        temp = all.map{|manager| manager.age}
+        temp.sum / temp.size.to_f
+    end
+
 end
