8f95856ae1a4e9d221e2b2617a939daae5812c28
soufianeou <soufiane.oucherrou@gmail.com>
2019-10-13 23:11:49 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,44 @@
 class Employee
 
+    attr_accessor :name, :salary, :manager
+
+    @@all = []
+    def initialize(name, salary, manager)
+        @name = name 
+        @salary = salary 
+        @manager = manager
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def manager_name
+        answer = ""
+        @@all.each do |element|
+            if element == self
+                answer  = element.manager.name
+            end
+        end
+        answer
+    end
+ 
+    def self.paid_over(arg)
+    @@all.select {|employee| employee.salary > arg}
+    end
+
+    def self.find_by_department(department_arg)
+        @@all.find do |employee|
+            if employee.manager.department == department_arg
+                employee
+            end
+        end
+    end
+    def tax_bracket
+    @@all.select do |employee|
+        (employee.salary - self.salary).abs < 1000
+    end 
+ end 
+
 end
