6e7bfc1f34e51dae9bbc741f1c7ed6b4ac0f8c45
github username <github email address>
2019-10-14 22:41:15 -0400

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,37 @@
 class Manager
 
+    attr_accessor :name, :department, :age
+
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def employees
+        Employees.all.select
+    end
+    
+    def hire_employee(employee_name, salary, manager)
+        Employee.new(employee_name, salary, self)
+    end
+
+    def self.all_departments
+        all.select do |manager|
+            manager.department == self
+        end
+    end
+    
+    def self.average_age
+        sum = all.reduce(0) { |sum, manager| sum + manager.age }
+        sum / all.length.to_f
+    end
+
 end
